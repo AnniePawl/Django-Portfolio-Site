a983a3dafd36d26eb83708463a1d053437dc76a8
Anna Pawl <annampawl@gmail.com>
2020-04-12 18:05:24 -0400

    added message board

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'portfolio'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact/', views.contact),
+    path('greet/<str:name>/', views.greet_by_name),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Portfolio Home Page")
+
+
+def contact(request):
+    return HttpResponse("Contact Me!")
+
+
+def greet_by_name(request, name):
+    # TODO: Return an HttpResponse that contains a string that includes the given name
+    return HttpResponse(f"Hello there, {name}!!!")
